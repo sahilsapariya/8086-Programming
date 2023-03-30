857e956a93ab39c46c35080f31d858a3a077c599
Sahil <sahilsapariya03@gmail.com>
2023-03-30 21:29:24 +0530

    created another program files

---
+++ ch3/3_11.asm
@@ -0,0 +1,26 @@
+;wap to perform ORing of two 16-bits number without using OR instruction.
+
+data segment
+    n1 dw 1234h
+    n2 dw 4321h
+    ans dw ?
+data ends
+code segment
+    assume cs:code, ds:data
+start:
+    mov ax, data
+    mov ds, ax
+
+    mov ax, n1
+    not ax
+    mov bx, n2
+    not bx
+
+    and ax, bx
+    not axd
+    mov ans, ax
+
+    mov ax, 4c00h
+    int 21h
+code ends
+end start
---
+++ ch3/3_9.asm
@@ -0,0 +1,32 @@
+; wap to perform scalar multiplication of array of five unsigned bytes.
+
+data segment
+    n db 03h
+    val db 12h, 0A5h, 0FFh, 34h, 98h
+    s_val dw 5 dup(0)
+data ends
+
+code segment
+    assume cs: code, ds: data
+start:
+    mov ax, data
+    mov ds, ax
+
+    mov cx, 5
+    mov bl, n
+    lea si, val
+    lea di, s_val
+
+next:
+    mov al, byte ptr [si]
+    mul bl
+    mov word ptr [di], ax
+    inc si
+    add di, 2
+    loop next
+
+    mov ax, 4c00h
+    int 21h
+code ends
+end start
+
